6b3a74484faa09c35658fcb890a47794372a23a5
Reno Raksi <adriano.raksi@gmail.com>
2025-04-20 21:32:49 -0500

    Added user profile picture to navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import TextButton from '@/components/TextButton';
-import { signOut } from '@/auth';
+import { auth, signOut } from '@/auth';
 
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await auth();
+  if (!session) {
+    return null;
+  }
+
   return (
       <nav className="w-screen flex items-center justify-between px-12 py-6">
         <Link href="/" className="flex items-center gap-2">
@@ -21,10 +26,13 @@ const Navbar = () => {
               'use server';
               await signOut();
             }}
+            className="flex items-center gap-8"
         >
           <TextButton type="submit">
             Sign Out
           </TextButton>
+          <Image src={session.user?.image as string} alt={`${session.user?.name}'s Profile Picture`} width={32}
+                 height={32} className="rounded-full" />
         </form>
       </nav>
   );
--- app/SignedIn.tsx
+++ app/SignedIn.tsx
@@ -1,17 +1,11 @@
-import { auth } from '@/auth';
 import Navbar from '@/app/Navbar';
 
 const SignedIn = async () => {
-  const session = await auth();
-  if (!session) {
-    return null;
-  }
-
   return (
       <div>
         <Navbar />
         <main className="flex flex-col m-auto max-w-[96rem] min-w-[64rem]">
-          <p>{JSON.stringify(session)}</p>
+          <p>Hello world!</p>
         </main>
       </div>
   );
