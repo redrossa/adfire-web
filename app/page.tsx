fbe1af340487560f9418e4b9df7437daea13a3fe
Reno Raksi <adriano.raksi@gmail.com>
2025-04-20 21:32:49 -0500

    Added a portfolio uploader

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,14 @@
-import Container from '@/components/Container';
-import overview from '../.reports/index.json';
-import CategoryTag from '@/components/CategoryTag';
-import AmountTag from '@/components/AmountTag';
+import PortfolioLoader from '@/components/PortfolioLoader';
+import PortfolioOverview from '@/components/PortfolioOverview';
 
-export default function Home() {
+export default async function Home() {
   return (
       <div className="flex flex-col items-center">
-        <div className="flex flex-col m-auto max-w-[96rem] min-w-[56rem]">
+        <div className="flex flex-col m-auto max-w-[96rem] min-w-[64rem]">
           <h2 className="mt-24 font-extrabold">Portfolio Overview</h2>
           <h4 className="mb-10">Find a snapshot about your portfolio here</h4>
-          <Container>
-            <div className="w-full px-10 py-16">
-              <h4 className="font-extrabold">Highlighted Transactions</h4>
-            </div>
-            {overview.map((item, i) => (
-                <div
-                    className="w-full p-10 grid grid-cols-5 grid-rows-2 gap-x-16 border-t border-solid border-neutral-200"
-                    key={i}
-                >
-                  <h6 className="place-content-end font-light">{item.date}</h6>
-                  <h5 className="place-content-end col-span-3 align-middle">{item.title}</h5>
-                  <AmountTag value={item.amount} isChange={item.type !== 'open'} />
-                  <div className="col-start-2 col-span-3 flex items-center gap-4">
-                    <small>{item.account}</small>
-                    {item.category && <CategoryTag category={item.category} />}
-                  </div>
-                </div>
-            ))}
-          </Container>
+          <PortfolioLoader />
+          <PortfolioOverview />
         </div>
       </div>
   );
