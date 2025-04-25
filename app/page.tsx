196ede89999b8d71a93579ef5070473267bd281c
Reno Raksi <adriano.raksi@gmail.com>
2025-04-25 16:32:01 -0500

    Updated navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
 import Landing from '@/app/Landing';
 import { auth } from '@/auth';
-import User from '@/app/_[user]/User';
 
 export default async function RootPage() {
   const session = await auth();
-  return !session?.user?.name ? <Landing /> : <User user={session.user.name} />;
+  return !session?.user?.name ? <Landing /> : <p>Home</p>;
 }
