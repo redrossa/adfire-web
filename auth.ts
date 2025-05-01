ec0d5a5aaae80decb5b23947ec9fc55e587158b1
Reno Raksi <adriano.raksi@gmail.com>
2025-05-01 11:43:53 -0500

    Set up postgres with auth

--- auth.ts
+++ auth.ts
@@ -1,6 +1,13 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import PostgresAdapter from '@auth/pg-adapter';
+import { Pool } from 'pg';
+
+const pool = new Pool({
+  connectionString: process.env.AUTH_DATABASE_URL
+});
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PostgresAdapter(pool),
   providers: [Google]
 });
