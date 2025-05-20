3544d2ad9f50cd1e78a497dc106650a0ae9b4f6b
Reno Raksi <adriano.raksi@gmail.com>
2025-05-19 23:21:06 -0500

    Merged

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,11 +1,13 @@
-export default function Footer() {
+const Footer = () => {
   return (
     <footer className="relative mt-16 py-8 md:mt-20">
       <div className="flex justify-between gap-2 max-sm:flex-col max-sm:text-center">
-        <p className="text-muted-foreground text-sm">
+        <p className="opacity-40 text-sm">
           &copy; {new Date().getFullYear()} Adfire
         </p>
       </div>
     </footer>
   );
-}
+};
+
+export default Footer;
--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,31 @@
 'use client';
 
-import Link from 'next/link';
+import NextLink from 'next/link';
 import Image from 'next/image';
-import { useIsMobile } from '@/hooks/useIsMobile';
+import { useIsMobile } from '@/lib/hooks/useIsMobile';
 import Logo from '@/public/vercel.svg';
 import LightLogo from '@/public/vercel-light.svg';
-import { cn, getInitials } from '@/lib/utils';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/avatar';
-import {
-  ArrowLeftRightIcon,
-  ChevronDownIcon,
-  LandmarkIcon,
-  LogOutIcon,
-  SunMoonIcon,
-} from 'lucide-react';
+import { getInitials } from '@/lib/utils';
 import { signOut, useSession } from 'next-auth/react';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@/components/dropdown-menu';
-import { ForwardRefExoticComponent } from 'react';
+import { ForwardRefExoticComponent, useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@heroui/dropdown';
+import { Avatar } from '@heroui/avatar';
+import { Link } from '@heroui/link';
+import { User } from '@heroui/user';
+import {
+  ArrowLeftStartOnRectangleIcon,
+  ArrowsRightLeftIcon,
+  BuildingLibraryIcon,
+  MoonIcon,
+  SunIcon,
+} from '@heroicons/react/24/outline';
+import { Button } from '@heroui/button';
 
 interface NavLink {
   Icon: ForwardRefExoticComponent<any>;
@@ -34,20 +34,18 @@ interface NavLink {
 }
 
 const navLinks: NavLink[] = [
-  { Icon: ArrowLeftRightIcon, text: 'Transactions', href: '/transactions' },
-  { Icon: LandmarkIcon, text: 'Accounts', href: '/accounts' },
+  { Icon: ArrowsRightLeftIcon, text: 'Transactions', href: '/transactions' },
+  { Icon: BuildingLibraryIcon, text: 'Accounts', href: '/accounts' },
 ];
 
-function Header() {
+const Header = () => {
   const isMobile = useIsMobile();
   const session = useSession();
   const { theme, setTheme } = useTheme();
+  const [prefersDarkScheme, setPrefersDarkScheme] = useState<boolean>();
 
   const smartToggle = () => {
     /* The smart toggle by @nrjdalal */
-    const prefersDarkScheme = window.matchMedia(
-      '(prefers-color-scheme: dark)',
-    ).matches;
     if (theme === 'system') {
       setTheme(prefersDarkScheme ? 'light' : 'dark');
     } else if (
@@ -60,169 +58,132 @@ function Header() {
     }
   };
 
+  useEffect(() => {
+    setPrefersDarkScheme(
+      window.matchMedia('(prefers-color-scheme: dark)').matches,
+    );
+  }, []);
+
   return (
     <header className="relative mb-14">
       <nav className="mx-auto flex h-[72px] w-full items-center justify-between gap-3">
-        <Link className="shrink-0" href="/" aria-label="Home">
+        <NextLink className="shrink-0" href="/" aria-label="Home">
           <span className="sr-only">Adfire</span>
           <Image
             src={Logo}
             alt="Adfire Logo"
-            width={24}
-            height={24}
             priority={true}
-            className="dark:hidden"
+            className="h-6 w-auto"
           />
           <Image
             src={LightLogo}
             alt="Adfire Logo"
-            width={24}
-            height={24}
             priority={true}
-            className="hidden dark:block"
+            className="hidden dark:block h-6 w-auto"
           />
-        </Link>
+        </NextLink>
         {session.status === 'authenticated' && (
-          <div className="flex items-center">
-            {!isMobile && (
-              <>
-                <div className="flex items-center gap-4 md:gap-10">
-                  {navLinks.map(
-                    (link) =>
-                      link.href && (
-                        <HeaderLink
-                          key={link.text}
-                          text={link.text}
-                          href={link.href}
-                        />
-                      ),
-                  )}
-                </div>
-                <div
-                  className="bg-input ms-4 me-4 h-5 w-px md:ms-10"
-                  aria-hidden="true"
-                ></div>
-              </>
-            )}
-            <div className="flex items-center gap-1">
+          <div className="flex items-center gap-4 md:gap-10">
+            {!isMobile &&
+              navLinks.map(
+                (link) =>
+                  link.href && (
+                    <Link
+                      as={NextLink}
+                      key={link.href}
+                      href={link.href}
+                      color="foreground"
+                    >
+                      {link.text}
+                    </Link>
+                  ),
+              )}
+            <Dropdown>
+              <DropdownTrigger>
+                <Button
+                  className="flex items-center gap-2"
+                  variant="light"
+                  isIconOnly
+                  radius="full"
+                  disableRipple
+                >
+                  <Avatar
+                    showFallback
+                    name={getInitials(session.data.user?.name || '')}
+                    src={session.data.user?.image as string}
+                    alt={session.data.user?.name as string}
+                  ></Avatar>
+                </Button>
+              </DropdownTrigger>
               <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <button className="flex items-center gap-2 cursor-pointer">
-                    <Avatar className="select-none">
-                      <AvatarImage
-                        src={session.data.user?.image as string}
-                        alt={session.data.user?.name as string}
-                      />
-                      {session.data.user?.name && (
-                        <AvatarFallback>
-                          {getInitials(session.data.user?.name)}
-                        </AvatarFallback>
-                      )}
-                    </Avatar>
-                    {!isMobile && (
-                      <ChevronDownIcon
-                        size={16}
-                        className="opacity-60"
-                        aria-hidden="true"
-                      />
-                    )}
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end" className="px-0">
-                  <DropdownMenuLabel className="flex min-w-0 flex-col">
-                    <span className="text-foreground truncate text-sm font-medium">
-                      {session.data.user?.name}
-                    </span>
-                    <small className="text-muted-foreground truncate text-xs font-normal">
-                      {session.data.user?.email}
-                    </small>
-                  </DropdownMenuLabel>
-                  {isMobile && (
-                    <>
-                      <DropdownMenuSeparator />
-                      <DropdownMenuGroup>
-                        {navLinks.map((link) => (
-                          <DropdownMenuItem
-                            key={link.href}
-                            className="cursor-pointer outline-hidden focus:underline"
-                            asChild
-                          >
-                            <HeaderLink
-                              text={link.text}
-                              href={link.href}
-                              Icon={link.Icon}
-                            />
-                          </DropdownMenuItem>
-                        ))}
-                      </DropdownMenuGroup>
-                    </>
-                  )}
-                  <DropdownMenuSeparator />
-                  <DropdownMenuGroup>
-                    <DropdownMenuItem
-                      className="cursor-pointer outline-hidden focus:underline"
-                      asChild
-                    >
-                      <div onClick={smartToggle}>
-                        <SunMoonIcon
-                          className="opacity-60"
-                          size={16}
-                          aria-hidden
-                        />
-                        <p>Switch themes</p>
-                      </div>
-                    </DropdownMenuItem>
-                    <DropdownMenuItem
-                      className="cursor-pointer outline-hidden focus:underline"
-                      asChild
-                      variant="destructive"
-                    >
-                      <div onClick={() => signOut()}>
-                        <LogOutIcon
-                          className="opacity-60"
-                          size={16}
-                          aria-hidden
-                        />
-                        <p>Sign out</p>
-                      </div>
-                    </DropdownMenuItem>
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
+                <DropdownItem
+                  key="profile"
+                  textValue="profile"
+                  showDivider
+                  isReadOnly
+                >
+                  <User
+                    avatarProps={{
+                      className: 'hidden',
+                    }}
+                    name={session.data.user?.name}
+                    description={session.data.user?.email}
+                  />
+                </DropdownItem>
+                {!isMobile ? null : (
+                  <>
+                    {navLinks.map((link) => (
+                      <DropdownItem
+                        as={NextLink}
+                        href={link.href}
+                        key={link.href}
+                        startContent={
+                          <link.Icon
+                            className="opacity-60 w-4 h-auto"
+                            aria-hidden
+                          />
+                        }
+                      >
+                        {link.text}
+                      </DropdownItem>
+                    ))}
+                  </>
+                )}
+                <DropdownItem
+                  key="theme"
+                  onClick={smartToggle}
+                  startContent={
+                    (theme === 'system' && prefersDarkScheme) ||
+                    theme === 'dark' ? (
+                      <SunIcon className="opacity-60 w-4 h-auto" aria-hidden />
+                    ) : (
+                      <MoonIcon className="opacity-60 w-4 h-auto" aria-hidden />
+                    )
+                  }
+                >
+                  Switch themes
+                </DropdownItem>
+                <DropdownItem
+                  key="signout"
+                  className="text-danger"
+                  color="danger"
+                  onClick={() => signOut()}
+                  startContent={
+                    <ArrowLeftStartOnRectangleIcon
+                      className="opacity-60 w-4 h-auto"
+                      aria-hidden
+                    />
+                  }
+                >
+                  Sign out
+                </DropdownItem>
               </DropdownMenu>
-            </div>
+            </Dropdown>
           </div>
         )}
       </nav>
     </header>
   );
-}
+};
 
-function HeaderLink({
-  text,
-  Icon,
-  href,
-  className,
-  ...props
-}: {
-  text: string;
-  Icon?: ForwardRefExoticComponent<any>;
-  href: string;
-  className?: string;
-  [key: string]: any;
-}) {
-  return (
-    <Link
-      href={href}
-      className={cn(
-        'flex items-center gap-0.5 text-sm hover:underline',
-        className,
-      )}
-      {...props}
-    >
-      {Icon && <Icon size={16} aria-hidden />}
-      <p>{text}</p>
-    </Link>
-  );
-}
-
-export { Header, HeaderLink };
+export default Header;
