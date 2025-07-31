c7a93e8ff053efd38d1c943031dc2efb2d8b2c49
Reno Raksi <adriano.raksi@gmail.com>
2025-07-31 11:52:26 -0500

    Remove viewport

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,12 @@
 import { fetchTransactionSummaryGroup } from '@/app/lib/queries/transactions';
 import TransactionsList from '@/app/ui/transactions/list';
-import { headers } from 'next/headers';
-import { userAgent } from 'next/server';
 
 export default async function DashboardPage() {
   const transactions = await fetchTransactionSummaryGroup();
-  const { device } = userAgent({ headers: await headers() });
-  const viewport = device.type ?? 'desktop';
   return (
     <>
       <h4 className="mb-4">Transactions</h4>
-      <TransactionsList transactions={transactions} viewport={viewport} />
+      <TransactionsList transactions={transactions} />
     </>
   );
 }
